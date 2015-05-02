85b40207e86bfcb4af8b6cb93293113800d75cbb
Robert <robertCamba@gmail.com>
2015-05-02 05:20:32 -0500

    refactored

--- resources/toDoList.py
+++ resources/toDoList.py
@@ -1,9 +1,6 @@
-#toDoList
-#CatFoodForPrawnGuns
-
 from sys import argv, exit as sys_exit
 from datetime import date
-from root import toDoListTextFile, switchBoard, printNumberedList, createBackUp, setClipboardData, chooseFromNumberedList
+from root import toDoListTextFile, switchParser, printList, createBackUp, setClipboardData, chooseFromList, errorAlert
 
 AVAILABLE_SWITCHES=['a','v','d','s']
 
@@ -21,10 +18,10 @@ class ToDoTask:
 	
 	def __str__(self):
 		return "".join([ self.getDate(), '\t', self.getTask() ])
-		#return "".join([ self.getDate(), '    ', self.getTask() ])
-
+		
 
 def addItem(newTask):
+	createBackUp(toDoListTextFile)
 	
 	newTask=ToDoTask(newTask)
 	writer=open(toDoListTextFile,'a')
@@ -32,11 +29,12 @@ def addItem(newTask):
 	writer.write( str("".join([newTask.getDate(),"\t",newTask.getTask()])) )
 	writer.close()
 
-def deleteItem(taskList,numberOfItem):
+def deleteItem(numberOfItem):
+	createBackUp(toDoListTextFile)
+	taskList=loadTaskList()
 	
 	print "Removing:\n\t", str(numberOfItem)+")", taskList[numberOfItem-1].getDate(), "\t", taskList[numberOfItem-1].getTask()
 	taskList.remove(taskList[numberOfItem-1])
-	
 	
 	writer=open(toDoListTextFile,'w')
 	for task in taskList:
@@ -46,23 +44,42 @@ def deleteItem(taskList,numberOfItem):
 				writer.write('\n')
 	writer.close()
 	
-	
-def viewToDoList(taskList):
-	for i in range(0,len(taskList)):
-		taskList[i]=taskList[i].__str__()
-	printNumberedList(taskList)	
-	
-if __name__=="__main__":
-	
-	switches=switchBoard(argv)
-	createBackUp(toDoListTextFile)
-	
-	
+def viewToDoList():
+	taskList=loadTaskList()
+	map(str,taskList)
+	printList(taskList)	
+
+def loadTaskList():
 	taskList=open(toDoListTextFile).read().split('\n')
 	for i in range(0,len(taskList)):
 		if len(taskList[i])>0:
 			taskList[i]=ToDoTask(taskList[i].split('\t')[1], taskList[i].split('\t')[0])
-		
+	
+	return taskList
+
+def handleSelect():
+	taskList=loadTaskList()
+	if 's' in switches:
+
+		try:
+			if len(switches['s'])>0:
+				result=taskList[ int(switches['s']) ].getTask()
+			else:
+				result=chooseFromList(taskList).getTask()
+				
+			print result
+			setClipboardData(result)
+		except IndexError:
+			raise
+			errorAlert( "Choice is out of bounds.")
+			
+		except ValueError:
+			errorAlert( "Value of 's' switch must be an integer" )
+
+
+if __name__=="__main__":
+	
+	switches=switchParser(argv)
 	
 	if 'a' in switches:
 		if len(argv)>1:
@@ -72,21 +89,11 @@ if __name__=="__main__":
 			if len(task)>1:
 				addItem(task)
 			
-	
 	elif 'd' in switches:
-		deleteItem(taskList, int(argv[1]))
+		deleteItem(int(argv[1]))
 	
 	elif 'v' in switches:
-		viewToDoList(taskList)
+		viewToDoList()
 		
-		if 's' in switches:
-		
-			try:
-				chosenNum=chooseFromNumberedList(taskList)
-				result=taskList[chosenNum].getTask()
-				#print result
-				setClipboardData(result)
-			except IndexError:
-				raise
-				print "Number ", argv[1], "  is out of bounds."
+		handleSelect()
 		
