20a6b2a4458475bad1183e71f72db3204c602e4c
Robert <robertCamba@gmail.com>
2015-05-27 23:50:06 -0500

    untag multiple files/tags

--- resources/untag.py
+++ resources/untag.py
@@ -1,21 +1,44 @@
 from tag import removeTags, getTagList,getFilenameList
 from sys import argv
-from root import printList, switchParser, chooseFromList
+from root import printList, switchParser, keyPressInput
 AVAILABLE_SWITCHES=['f','t']
+	
+def _chooseFromList(aList):
+	choiceList=[]
+	if len(aList)>1:
+		print "Enter number(s) separated by commas"
+		
+		try:
+			input= raw_input()
+		except EOFError:#pipes
+			input=keyPressInput()
+				
+		choices=input.split(',')
+		choices=map(int,choices)
+		
+		for choice in choices:
+			choiceList.append( aList[choice-1] )
+	
+	else:
+		choiceList.append( aList[0] )
+		
+	return choiceList
 	
 def untagUsingTag(tag):
 	fList=getFilenameList( tag )
 	if len(fList)>0:
 		printList(fList)
-		choiceFile=chooseFromList(fList)
-		removeTags([tag], choiceFile)
+		choiceList=_chooseFromList(fList)
+		for choiceFile in choiceList:
+			removeTags([tag], choiceFile)
 
 def untagUsingFilename(filename):
 	tagList=getTagList(filename)
 	if len(tagList)>0:
 		printList(tagList)
-		choiceTag=chooseFromList(tagList)
-		removeTags([choiceTag], filename)
+		choiceList=_chooseFromList(tagList)
+		for choiceTag in choiceList:
+			removeTags([choiceTag], filename)
 
 if __name__ == "__main__":
 	switches=switchParser(argv, AVAILABLE_SWITCHES)
