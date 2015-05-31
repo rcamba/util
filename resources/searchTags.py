d75078731ba91b7ddc41a994695acd550fc6a007
Robert <robertCamba@gmail.com>
2015-05-31 13:15:14 -0500

    weak regex stuff e.g: search s* displays all tags beginning with s search *s* will display all tags that contains s

--- resources/searchTags.py
+++ resources/searchTags.py
@@ -5,61 +5,79 @@ from sys import argv, stdin, stdout
 from root import switchParser, printList, setClipboardData, chooseFromList, pipedList
 from tag import getFilenameList, getTagList
 from string import strip
+import re
 AVAILABLE_SWITCHES=['s','f']
 
 def main(argList):
 	if 'f' in switches:
-			
+
 		tagList=getTagList(argList[0])
 		print tagList
+
+	elif any([arg for arg in argList if '*' in arg]):
+		#... just one for now
+		pattern=[arg for arg in argList if '*' in arg][0].replace("*","\w+")
+		pattern="^"+pattern
+
+		pattern = re.compile(pattern)
+		tList=getTagList()
+		for tag in tList[:]	:
+			if pattern.search(tag)==None:
+				tList.remove(tag)
+
+		chooseFromTags(tList)
 
 	else:
 		tagList= " ".join(map(str, argList)).split(',')
 		tagList=map(strip,tagList)
 		fileList = getFilenameList( tagList)
-		
+
 		for i in range(0,len(fileList)):
 			fileList[i]="\""+fileList[i]+"\""
-		
+
 		if 's' in switches:
 			if len(switches['s'])>0:
 				choice=fileList[int (switches['s'])-1]
-			
+
 			else:
 				printList( fileList, pressToContinue=stdout.isatty() )
-			
+
 				if len(fileList)==1:
 					choice=fileList[0]
-				
+
 				else:
 					choice=chooseFromList(fileList)
-				
+
 			print choice
 			setClipboardData(choice)
-		
+
 		else:
 			printList( fileList, pressToContinue=stdout.isatty() )
-		
+
+
+def chooseFromTags(tList):
+	printList(tList)
+	choice=chooseFromList( tList )
+	cfList=  getFilenameList(choice)
+	printList(cfList)
+	choice=chooseFromList( cfList)
+	setClipboardData ( choice)
+
 if __name__=="__main__":
 	switches=switchParser(argv)
-	
-	
+
+
 	if stdin.isatty()==False:#for using with nf/search
 		print "Piped search"
 		argList=pipedList( "".join(map(str,stdin.readlines())) )
 		main(argList)
-	
-	elif len(argv)>1:	
+
+	elif len(argv)>1:
 		main(argv[1:])
-		
-			
+
+
 	else:
-		
+
 		tList=getTagList()
 		tList.sort()
-		printList(tList)
-		choice=chooseFromList( tList )
-		cfList=  getFilenameList(choice)
-		printList(cfList)
-		choice=chooseFromList( cfList)
-		setClipboardData ( choice)
+		chooseFromTags(tList)
