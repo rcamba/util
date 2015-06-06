51437b129cb331b90de1cef9af44e8842210d2e0
Robert <robertCamba@gmail.com>
2015-06-05 23:04:25 -0500

    cleanup

--- resources/dirJump.py
+++ resources/dirJump.py
@@ -12,25 +12,25 @@ def writeToPrevDirFile(prevDir):
 	f.close()
 
 def sortDirJump(targPosition):
-	
+
 	jumpList=getJumpList()
 	jumpList[0]= jumpList[targPosition]
 	writeToPrevDirFile(getcwd())
 	writeDirJump(jumpList)
-	
+
 def addToDirJump(targDir):
 	createBackUp(dirJumpFile)
 	print "Adding: " + targDir
 	f=open(dirJumpFile,"a")
 	f.write(str( getcwd() ) )
-	f.close()	
-	
+	f.close()
+
 def removeFromDirJump(targPosition):
 	jumpList=getJumpList()
 	print "Removing: " + jumpList[targPosition]
 	jumpList.remove(jumpList[targPosition])
 	writeDirJump(jumpList)
-	
+
 def writeDirJump(fList):
 	#createBackUp(dirJumpFile) - don't want to back it up every single jump
 	f=open(dirJumpFile,"w")
@@ -38,35 +38,35 @@ def writeDirJump(fList):
 		f.write(file)
 		f.write("\n")
 	f.close()
-	
+
 def getJumpList():
 	f=open(dirJumpFile,"r")
 	lineList=f.readlines()
 	f.close()
-	
+
 	lineList=map(strip,lineList)
-	
+
 	return lineList
-	
+
 if __name__ == "__main__":
 	switches=switchParser(argv)
 	if 'a' in switches:
 		addToDirJump(getcwd())
-		
+
 	elif 'd' in switches:
 		try:
 			targPosition=int(switches['d'])
 		except ValueError:
 			errorAlert("-d must contain int selecting directory to be removed from jump list")
 		removeFromDirJump(targPosition)
-	
+
 	elif len(argv)>1:
 		try:
 			targPosition=int(argv[1])
 		except ValueError:
-			errorAlert("# out of directory listing")
+			errorAlert("# out of directory listing", True)
 		sortDirJump(targPosition)
-	
+
 	else:
 		jumpList= getJumpList()
 		printList( jumpList[1:] )
