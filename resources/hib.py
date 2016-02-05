e6da1cde9d3066611d1fd4bccc600bfb98b12512
Robert <robertCamba@gmail.com>
2016-02-04 22:34:01 -0600

    use shutdown /h instead of PowrProf cleanedup

--- resources/hib.py
+++ resources/hib.py
@@ -1,91 +1,97 @@
+from sys import stdout, argv
+from time import sleep, time, strftime, localtime
+from root import hibLog, errorAlert
+from toDoList import viewToDoList
+from datetime import datetime
+from ctypes import windll
+from os import system
 
 
-def loadingSplash(givenTime, output="", splash=['.', "..", "..."]):
-	from sys import stdout
-	from time import sleep, time
-	
-	def writeSlashB(string):
-		result=""
+def loadingSplash(timeLimit, output="", splash=['', '.', "..", "..."]):
+
+	def backspaceStdout(string):
+		result = ""
 		for i in range(0,len(string)):
-			result=result+"\b"
-		stdout.write(result)	
+			result += "\b"
+		stdout.write(result)
 
-	def clearOut(string):
-		clear=""
+	def clearStdOut(string):
+		clear =""
 		for i in range(0,len(string)):
-			clear=clear+" "
+			clear = clear + " "
 		stdout.write(clear)
-		
+
 		return clear
-	
-	#givenTime in seconds
-	
-	i=0	
+
+	i = 0
 	stdout.write(output)
-	startTime=time()
-	while (time()-startTime) < givenTime :
-		
-		
-		splashText=splash[i]
+	startTime = time()
+	while (time()-startTime) < timeLimit :
+
+		splashText = splash[i]
 		stdout.write(splashText)
-		sleep(0.5)
+		sleep(1)
 		stdout.flush()
-		
-		writeSlashB(splashText)
-		
-		i=i+1
-		if(i==len(splash)):
-			writeSlashB(clearOut(splashText))
-			i=0
-			
+
+		backspaceStdout(splashText)
+
+		i += 1
+		if i == len(splash):
+			backspaceStdout(clearStdOut(splashText))
+			i = 0
+
 	stdout.flush()
-	
-	writeSlashB(output)
+	backspaceStdout(output)
+
 
 def createLog():
-	"""Opens hibLog.log and appends date and time to the file"""
-	from root import hibLog
-	from time import strftime, localtime
-	f=open(hibLog,"a")
-	t=localtime()
-	s=" ".join([str(strftime("%B/%d/%Y\t %H:%M", t)),"\n"])
+	"""Opens hibLog and appends date and time to the file"""
+
+	f = open(hibLog, 'a')
+	t = localtime()
+	s = " ".join([str(strftime("%B/%d/%Y\t %H:%M", t)), '\n'])
 	f.write(s)
 	f.close()
 
-def main(setTime):	
-	
+
+def hibernate():
+
 	"""
-	Sets program to sleep for setTime minutes
-	Log is created after sleep
-	Calls toDoList for ready viewing once system resumes from hibernation 
+	hib_cmd = r"C:\Windows\System32\rundll32.exe " +
+	"PowrProf.dll,SetSuspendState"
+	"""
+
+	hib_cmd = "shutdown /h"
+	system(hib_cmd)
+	viewToDoList()
+
+
+def main(timeLimit):
+	"""
+	Sets program to sleep for timeLimit minutes
+	Hibernate log is created after sleep with time stamp
+	Calls toDoList to be viewable when resuming from hibernation
 	Sets the computer to hibernate
 	"""
-	
-	for i in range(int(setTime), 0, -1 ):
-		output="Time remaining until hibernation: "+ str( i)
+
+	for i in range(int(timeLimit), 0, -1):
+		output = "Time remaining until hibernation: {}".format(str( i))
 		loadingSplash(60, output)
-	
-	
+
 	createLog()
 	hibernate()
-	
-def hibernate():
-	from toDoList import viewToDoList
-	system(r"C:\Windows\System32\rundll32.exe PowrProf.dll,SetSuspendState")#hibernate
-	viewToDoList()
-	
-	
+
+
 if __name__ == "__main__":
-	from sys import argv
-	from os import system
-	from ctypes import windll
-	windll.kernel32.SetConsoleCtrlHandler(0, 1)
-	if(len(argv)<2):
-		print "Missing time parameter"
+
+	#windll.kernel32.SetConsoleCtrlHandler(0, 1)  # disables sigint / ctrl + c
+
+	if(len(argv) < 2):
+		errorAlert("Missing time parameter")
+
 	else:
 		try:
 			main(int(argv[1]))
-			
+
 		except ValueError:
 			errorAlert("Argument must be an integer")
-		
