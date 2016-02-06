c1b1e512a65a27a47b111bfcecda75c5c99b232a
Robert <robertCamba@gmail.com>
2016-02-06 16:53:17 -0600

    refactor p2: multi vids

--- resources/getNewMusicX2.py
+++ resources/getNewMusicX2.py
@@ -7,8 +7,9 @@ from sys import argv
 from cleanFileNames import cleanString
 from urllib2 import quote
 from subprocess import Popen
+from random import randint
 
-from root import screeningDir, musicDir, switchBoard, getAllPageLinks, ytDownloadsDir, ytAMVDir, outputFromCommand, errorAlert, deletedScreenedLog
+from root import screeningDir, musicDir, switchBoard, getAllPageLinks, yt_dls_dir, yt_amv_dir, outputFromCommand, errorAlert, deletedScreenedLog
 
 
 
@@ -21,74 +22,78 @@ from root import screeningDir, musicDir, switchBoard, getAllPageLinks, ytDownloa
 MAX_TRIES=3
 AVAILABLE_SWITCHES=['v','h','s','m']
 
-# YT_DL_PROG = "C:\\Users\\Kevin\\Util\\youtubeDL.py"
 YT_DL_PROG = "C:\\Users\\Kevin\\Downloads\\youtube-dl.exe"
 
+def rand_sleep():
+	sleep(randint(1,3))
 
-def getTitleFromSys(ytVidLink):
 
-	commandArgs="".join([ytVidLink , " -q -s --get-filename --extract-audio --restrict-filenames --output %(title)s_%(id)s.%(ext)s "])
-	title=outputFromCommand(YT_DL_PROG+" " + commandArgs)
+def get_dry_title(vid_link):
 
+	yt_dl_opts = "-q -s --get-filename --extract-audio --restrict-filenames --output ".split()
+	output_format = "%(title)s_%(id)s.%(ext)s"
+	title_cmd = [YT_DL_PROG, vid_link] + yt_dl_opts + [output_format]
+	title = outputFromCommand(title_cmd)
 	return title
 
-def getVidList(linksList):
-	vidList=[]
-	for link in linksList:
 
-		tempList=__getVidList( getAllPageLinks(link.replace('\\','/') ) )
+def get_vid_list(links_list):
+
+	vid_list = []
+	for link in links_list:
+
 		print "Retrieving video list from: ", link
 
-		i=0
-		while( len(tempList)==0 and i<MAX_TRIES):
-			vidLinks=getAllPageLinks(link.replace('\\','/'))
+		i = 0
+		page_links = getAllPageLinks(link)
 
-			tempList=__getVidList(vidLinks)
-
-			i=i+1
+		while len(page_links) <= 1 and i < MAX_TRIES:
+			page_links = getAllPageLinks(link)
+			i += 1
 			print "Retry # ", i
+			rand_sleep()
 
-			sleep(3)
-		vidList.extend(tempList)
+		if len(page_links) > 1:
+			vid_list.extend(parse_yt_links(page_links))
 
-	return vidList
+	return vid_list
 
-def __getVidList(vidLinks):
 
-	vHashList=[]
+def parse_yt_links(page_links):
 
-	for i in range(len(vidLinks)-1,-1,-1):
+	v_ID_list = []
+	for i in range(len(page_links) - 1, -1, -1):
 
-		vidLinks[i]=vidLinks[i].get("href")
-		if ( vidLinks[i] is not None):
+		page_links[i] = page_links[i].get("href")
+		if page_links[i] is not None:
 
-			if "youtube" not in vidLinks[i]:
-				vidLinks[i]="".join(["youtube.com",vidLinks[i]])
+			if "youtube" not in page_links[i]:
+				page_links[i] = "youtube.com" + page_links[i]
 
-			if  "http" not in vidLinks[i] :
-				vidLinks[i]="".join(["http://",vidLinks[i]])
-
-			vidLinks[i]=vidLinks[i].replace('\"','').replace('\\','/').replace(" ","")
+			if "http" not in page_links[i] :
+				page_links[i] = "http://" + page_links[i]
 
 			try:
-				preHash=vidLinks[i][:vidLinks[i].index("watch?")+6]
-				vHash=vidLinks[i][vidLinks[i].index('v='):]
-				vidLinks[i]="".join([preHash,vHash])
+				base_url = page_links[i][:page_links[i].index("watch?") + 6]
+				v_ID = page_links[i][page_links[i].index("v="):]
+				page_links[i] = base_url + v_ID
 
-				if vHash in vHashList:
-					vidLinks.remove(vidLinks[i])
-				else:
-					vHashList.append(vHash)
+				# try and slice off '&' options in the link
+				# e.g: watch?v=IDHERE&list=PLAYLIST
+				# slices off "&list=PLAYLIST"
+				if page_links[i].find('&') > -1:
+					page_links[i] = page_links[i][:page_links[i].index('&')]
 
-				try:
-					vidLinks[i]=vidLinks[i][:vidLinks[i].index('&')]
-				except:
-					pass
+				# prevent duplicates
+				if v_ID	 not in v_ID_list:
+					v_ID_list.append(v_ID)
+					page_links.remove(page_links[i])
 
-			except:
-				vidLinks.remove(vidLinks[i])
+			except (ValueError, IndexError):
+				page_links.remove(page_links[i])
 
-	return vidLinks
+	return page_links
+
 
 def alreadyDownloaded(title, targDir):
 	retVal=False
@@ -102,17 +107,17 @@ def alreadyDownloaded(title, targDir):
 
 	return retVal
 
-def downloadMusic(ytVidLink, targetDir):
+def downloadMusic(vid_link, target_dir):
 
-	command="".join([YT_DL_PROG, ytVidLink , " --quiet --restrict-filenames --no-mtime --no-overwrites --extract-audio --output \"",targetDir,"\\%(title)s_%(id)s.%(ext)s\""])
-	title=getTitleFromSys(ytVidLink)
+	command="".join([YT_DL_PROG, vid_link , " --quiet --restrict-filenames --no-mtime --no-overwrites --extract-audio --output \"",target_dir,"\\%(title)s_%(id)s.%(ext)s\""])
+	title=get_dry_title(vid_link)
 	if len(title)>0:
 
 		title=cleanString(title)
 		if alreadyDownloaded(title, musicDir)==False:#check musicDir instead of screening dir since there's no overwrites anyway
 			if path.splitext(title.lower())[0] not in deletedMusicList:
 				print "Downloading:", title
-				title="".join([targetDir,"\\",title])
+				title="".join([target_dir,"\\",title])
 
 				system(command)
 
@@ -128,40 +133,55 @@ def downloadMusic(ytVidLink, targetDir):
 		else:
 			errorAlert( title + " already in main music directory" )
 	else:
-		errorAlert( "Empty title for " + ytVidLink)
+		errorAlert( "Empty title for " + vid_link)
 
-def dl_single_video(ytVidLink, targetDir):
+def dl_single_video(vid_link, target_dir):
 
-	YT_DL_OPTS = "--quiet --rate-limit 100m  --no-mtime --no-overwrites --output".split()
-	YT_DL_OUTPUT_FILE = "{targDir}\\%(title)s_%(id)s.%(ext)s".format(targDir=targetDir)
+	yt_dl_opts = "--quiet --rate-limit 100m  --no-mtime --no-overwrites --output".split()
+	YT_DL_OUTPUT_FILE = "{targDir}\\%(title)s_%(id)s.%(ext)s".format(targDir=target_dir)
 
-	dl_vid_cmd = [YT_DL_PROG, ytVidLink] + YT_DL_OPTS + [YT_DL_OUTPUT_FILE]
+	dl_vid_cmd = [YT_DL_PROG, vid_link] + yt_dl_opts + [YT_DL_OUTPUT_FILE]
 
-	title = getTitleFromSys(ytVidLink)
+	title = get_dry_title(vid_link)
 	print "Downloading:", title
 
 	proc = Popen(dl_vid_cmd)
 
 
+def dl_multi_video(vid_link="http://www.reddit.com/r/amv"):
+
+	print "Downloading multiple videos from {}".format(vid_link)
+
+	vid_list = get_vid_list([vid_link])
+
+	for vid in vid_list:
+		if vid is not None:
+			dl_single_video(vid, yt_amv_dir)
+
+
 if __name__ == "__main__":
 
-	vidList=[]
+	vid_list=[]
 
 	char_func_mapping = {
-		's': lambda: dl_single_video(vid_link, ytDownloadsDir),
-		'v': lambda: dl_multi_video(),
-		'm': lambda: dl_sing_song(),
+		's': lambda: dl_single_video(vid_link, yt_dls_dir),
+		'v': lambda: dl_multi_video(vid_link) if len(vid_link) > 0 else dl_multi_video(),
+		'm': lambda: dl_single_song(),
 		'': lambda: dl_multi_song()
 	}
 
 	opt = argv[1].replace('-', '')
-	vid_link = quote(argv[2], safe="%/:=&?~#+!$,;'@()*[]")
+	vid_link = ""
+	if len(argv) > 2:
+		vid_link = quote(argv[2], safe="%/:=&?~#+!$,;'@()*[]")
+
 	char_func_mapping[opt]()
+
 	"""
 	if 's' in switchList:  # single video
 		print "Dowloading single video"
-		vidLink=" ".join(map(str,argv[1:])).replace('\\','/')
-		downloadVideo(vidLink,ytDownloadsDir)
+		vid_link=" ".join(map(str,argv[1:])).replace('\\','/')
+		downloadVideo(vid_link,yt_dls_dir)
 
 	elif 'v' in switchList:#multiple video
 		print "Downloading multiple videos from source"
@@ -170,18 +190,18 @@ if __name__ == "__main__":
 		if(len(argv)>1):
 			DEFAULT_LINKS=argv[1:]
 
-		vidList=getVidList(DEFAULT_LINKS)
+		vid_list=get_vid_list(DEFAULT_LINKS)
 
-		for vidLink in vidList:
-			if(vidLink is not None):
-				downloadVideo(vidLink,ytAMVDir)
+		for vid_link in vid_list:
+			if(vid_link is not None):
+				downloadVideo(vid_link,yt_amv_dir)
 
 
 	elif 'm' in switchList:#single music
 		print "Downloading single music"
 		deletedMusicList=open(deletedScreenedLog).read().split("\n")
-		vidLink=" ".join(map(str,argv[1:])).replace('\\','/')
-		downloadMusic(vidLink,musicDir)
+		vid_link=" ".join(map(str,argv[1:])).replace('\\','/')
+		downloadMusic(vid_link,musicDir)
 
 	else:#multiple music
 
@@ -190,12 +210,12 @@ if __name__ == "__main__":
 		if(len(argv)>1):
 			DEFAULT_LINKS=argv[1:]
 
-		vidList=getVidList(DEFAULT_LINKS)
+		vid_list=get_vid_list(DEFAULT_LINKS)
 		deletedMusicList=open(deletedScreenedLog).read().split("\n")
 
-		for vidLink in vidList:
-			if(vidLink is not None):
-				#downloadMusic(vidLink,screeningDir)
-				Thread(target=downloadMusic,args=(vidLink,screeningDir,)).start()
+		for vid_link in vid_list:
+			if(vid_link is not None):
+				#downloadMusic(vid_link,screeningDir)
+				Thread(target=downloadMusic,args=(vid_link,screeningDir,)).start()
 				sleep(3)
 	"""
