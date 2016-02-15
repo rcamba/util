20f3b4da9bb6386b865ceb6b389939914cc46f4c
Robert <robertCamba@gmail.com>
2016-02-15 00:10:29 -0600

    pep8

--- resources/compileALL.py
+++ resources/compileALL.py
@@ -1,4 +1,5 @@
 if __name__ == "__main__":
-	import compileall, os
-	compileall.compile_dir( os.getcwd(), -1)
-	os.system("del compileALL.pyc")
+    import compileall
+    import os
+    compileall.compile_dir(os.getcwd(), -1)
+    os.system("del compileALL.pyc")
