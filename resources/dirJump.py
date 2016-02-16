b6fb5b3eaed0dd6ff5da28fe5a27813be9fe529b
Robert <robertCamba@gmail.com>
2016-02-15 20:41:27 -0600

    pep8 + use "with" context mngr on open()

--- resources/dirJump.py
+++ resources/dirJump.py
@@ -1,74 +1,90 @@
-from root import dirJumpFile, printList, chooseFromList, prevDirFile, switchParser, createBackUp, errorAlert
+"""
+Used with ql.bat
+QL reads from the jump file and changes to the directory written in that file
+Required to do it from cmd/bat since python process is separate from cmd
+"""
+from root import dirJumpFile, printList, chooseFromList, prevDirFile, \
+    switchParser, createBackUp, errorAlert
 from sys import argv
-from string import lower, strip
+from string import strip
 from os import getcwd
 
 
-AVAILABLE_SWITCHES=['a','d']
+AVAILABLE_SWITCHES = ['a', 'd']
 
-def writeToPrevDirFile(prevDir):
-	f=open(prevDirFile,"w+")
-	f.write(prevDir)
-	f.close()
 
-def sortDirJump(targPosition):
+def write_to_prev_dir_file(prev_dir):
 
-	jumpList=getJumpList()
-	jumpList[0]= jumpList[targPosition]
-	writeToPrevDirFile(getcwd())
-	writeDirJump(jumpList)
+    with open(prevDirFile, "w+") as f:
+        f.write(prev_dir)
 
-def addToDirJump(targDir):
-	createBackUp(dirJumpFile)
-	print "Adding: " + targDir
-	f=open(dirJumpFile,"a")
-	f.write(str( getcwd() ) )
-	f.close()
 
-def removeFromDirJump(targPosition):
-	jumpList=getJumpList()
-	print "Removing: " + jumpList[targPosition]
-	jumpList.remove(jumpList[targPosition])
-	writeDirJump(jumpList)
+def sort_dir_jump(targ_pos):
 
-def writeDirJump(fList):
-	#createBackUp(dirJumpFile) - don't want to back it up every single jump
-	f=open(dirJumpFile,"w")
-	for file in fList:
-		f.write(file)
-		f.write("\n")
-	f.close()
+    jump_list = get_jump_list()
+    jump_list[0] = jump_list[targ_pos]
+    write_to_prev_dir_file(getcwd())
+    write_to_dir_jump(jump_list)
 
-def getJumpList():
-	f=open(dirJumpFile,"r")
-	lineList=f.readlines()
-	f.close()
 
-	lineList=map(strip,lineList)
+def add_to_dir_jump(targ_dir):
 
-	return lineList
+    createBackUp(dirJumpFile)
+    print "Adding: " + targ_dir
+    with open(dirJumpFile, "a") as f:
+        f.write(str(getcwd()))
+
+
+def remove_from_dir_jump(targ_pos):
+
+    jump_list = get_jump_list()
+    print "Removing: " + jump_list[targ_pos]
+    jump_list.remove(jump_list[targ_pos])
+    createBackUp(dirJumpFile)
+    write_to_dir_jump(jump_list)
+
+
+def write_to_dir_jump(flist):
+
+    with open(dirJumpFile, "w") as out:
+        for f in flist:
+            out.write(f)
+            out.write("\n")
+
+
+def get_jump_list():
+
+    lines = []
+    with open(dirJumpFile, "r") as f:
+        lines = f.readlines()
+        lines = map(strip, lines)
+
+    return lines
 
 if __name__ == "__main__":
-	switches=switchParser(argv)
-	if 'a' in switches:
-		addToDirJump(getcwd())
 
-	elif 'd' in switches:
-		try:
-			targPosition=int(switches['d'])
-		except ValueError:
-			errorAlert("-d must contain int selecting directory to be removed from jump list")
-		removeFromDirJump(targPosition)
+    switches = switchParser(argv)
+    if 'a' in switches:
+        add_to_dir_jump(getcwd())
 
-	elif len(argv)>1:
-		try:
-			targPosition=int(argv[1])
-		except ValueError:
-			errorAlert("# out of directory listing", True)
-		sortDirJump(targPosition)
+    elif 'd' in switches:
+        try:
+            targ_pos = int(switches['d'])
+        except ValueError:
+            errorAlert(
+                "-d must contain int for dir to be deleted from jump list")
+        remove_from_dir_jump(targ_pos)
 
-	else:
-		jumpList= getJumpList()
-		printList( jumpList[1:] )
-		choice=chooseFromList( jumpList[1:])
-		sortDirJump(jumpList.index(choice))#+1 for skipping first line of jumped route
+    elif len(argv) > 1:
+        try:
+            targ_pos = int(argv[1])
+        except ValueError:
+            errorAlert("# out of directory listing", True)
+        sort_dir_jump(targ_pos)
+
+    else:
+        jumpList = get_jump_list()
+        printList(jumpList[1:])
+        choice = chooseFromList(jumpList[1:])
+        # +1 for skipping first line of jumped route
+        sort_dir_jump(jumpList.index(choice))
