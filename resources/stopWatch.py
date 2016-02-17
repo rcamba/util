10aa987d66a7e39822e0aef9bd20b443cccecfee
Robert <robertCamba@gmail.com>
2016-02-16 23:53:56 -0600

    pep8 + refactor. weird kbhit is False issue.

--- resources/stopWatch.py
+++ resources/stopWatch.py
@@ -1,52 +1,49 @@
-#stopWatch
 from msvcrt import kbhit, getch
 from sys import stdout
-from collections import OrderedDict
-from datetime import datetime, time
+from datetime import datetime
+
 
 def wait_for_keypress():
-	if (kbhit() == False):
-		getch()
 
-	return True
+    if kbhit() is False:
+        getch()
+
+    return True
 
 
 def create_time_diff_str(init_time):
-	DECIMAL_ROUNDING = 1
-	TIME_FORMAT = "%H %M"
-	time_diff = datetime.now() - init_time
-	time_diff_str = str(time_diff)
 
-	return time_diff_str
+    time_diff = datetime.now() - init_time
+    time_diff_str = str(time_diff)
+
+    return time_diff_str
 
 
 def print_time(init_time):
 
-	prev_time_diff_str = ""
-	while kbhit() == False:
+    prev_time_diff_str = ""
 
-		time_diff_str = create_time_diff_str(init_time)
-		if time_diff_str != prev_time_diff_str:
-			stdout.write(time_diff_str + len(time_diff_str) * " " + "\r")
-			stdout.write(len(time_diff_str) * "\b")
-			stdout.flush()
-			prev_time_diff_str = time_diff_str
-
-	else:
-		getch()
+    # while kbhit() is False: # This doesn't work???
+    while kbhit() == False:
+        time_diff_str = create_time_diff_str(init_time)
+        if time_diff_str != prev_time_diff_str:
+            stdout.write(time_diff_str + len(time_diff_str) * " " + "\r")
+            stdout.write(len(time_diff_str) * "\b")
+            stdout.flush()
+            prev_time_diff_str = time_diff_str
+    getch()
 
 
 def main():
 
-	print "Press any key to START the timer"
-	wait_for_keypress()
+    print "Press any key to START the timer"
+    wait_for_keypress()
 
-	init_time = datetime.now()
+    init_time = datetime.now()
 
-	print "Press any key to STOP the timer"
-	print_time(init_time)
-
+    print "Press any key to STOP the timer"
+    print_time(init_time)
 
 
 if __name__ == "__main__":
-	main()
+    main()
