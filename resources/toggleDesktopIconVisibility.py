dda97d6b7ccedbd34520f2f6b935eaab6c31236b
Robert <robertCamba@gmail.com>
2016-02-16 23:07:03 -0600

    pep8 + refactor. removed zPolling - resolved issue.

--- resources/toggleDesktopIconVisibility.py
+++ resources/toggleDesktopIconVisibility.py
@@ -1,12 +1,15 @@
 from win32com import client
-from mouseMacro import rightclick
-from time import sleep
-from os import system
+from mouse_macro import rightclick
+from subprocess import Popen
+from os import path, getenv
 
 if __name__ == "__main__":
-	
-	system (r""" "%userprofile%\\"Application Data"\\Microsoft\\"Internet Explorer"\\"Quick Launch"\\"Shows Desktop.lnk"" """)
-	
-	rightclick()
-	shell=client.Dispatch("WScript.Shell")
-	shell.SendKeys("vd")
+
+    command = path.join(getenv("userprofile"), "Application Data", "Microsoft",
+                        "Internet Explorer", "Quick Launch",
+                        "Shows Desktop.lnk")
+    Popen(command)
+
+    rightclick()
+    shell = client.Dispatch("WScript.Shell")
+    shell.SendKeys("vd")
