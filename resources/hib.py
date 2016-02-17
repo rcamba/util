610cd5665e95931ab0501a82ca34024b6fe89198
Robert <robertCamba@gmail.com>
2016-02-16 22:20:43 -0600

    pep8 and added function comments

--- resources/hib.py
+++ resources/hib.py
@@ -2,96 +2,111 @@ from sys import stdout, argv
 from time import sleep, time, strftime, localtime
 from root import hibLog, errorAlert
 from toDoList import viewToDoList
-from datetime import datetime
-from ctypes import windll
 from os import system
 
 
-def loadingSplash(timeLimit, output="", splash=['', '.', "..", "..."]):
+def loading_splash(time_limit, output="", splash=['', '.', "..", "..."]):
+    """
 
-	def backspaceStdout(string):
-		result = ""
-		for i in range(0,len(string)):
-			result += "\b"
-		stdout.write(result)
+    :param time_limit: number of minutes that the splash will run for
+    :param output: static text for splash
+    :param splash: list of string that will be used as splash
 
-	def clearStdOut(string):
-		clear =""
-		for i in range(0,len(string)):
-			clear = clear + " "
-		stdout.write(clear)
+    How writing to stdout works:
+    output -> this is output plus the first splash string which is empty
+    output. -> output + '.' (i.e second splash string)
+    output -> output + \b  (move cursor to previous char)
+    output.. -> output. + ".." ('.' from second splash string is overwritten)
 
-		return clear
+    at the end clear the entire splash strings by overwriting with " " char
+    then set cursor back to the end of output + 1
+    """
 
-	i = 0
-	stdout.write(output)
-	startTime = time()
-	while (time()-startTime) < timeLimit :
+    def backspace_stdout(string):
+        result = ""
+        for j in range(0, len(string)):
+            result += "\b"
+        stdout.write(result)
 
-		splashText = splash[i]
-		stdout.write(splashText)
-		sleep(1)
-		stdout.flush()
+    def clear_std_out(string):
+        clear = ""
+        for j in range(0, len(string)):
+            clear += " "
+        stdout.write(clear)
 
-		backspaceStdout(splashText)
+        return clear
 
-		i += 1
-		if i == len(splash):
-			backspaceStdout(clearStdOut(splashText))
-			i = 0
+    stdout.write(output)
 
-	stdout.flush()
-	backspaceStdout(output)
+    i = 0
+    start_time = time()
+    while (time() - start_time) < time_limit:
+
+        splash_txt = splash[i]
+        stdout.write(splash_txt)
+        sleep(1)
+        stdout.flush()
+
+        backspace_stdout(splash_txt)
+
+        i += 1
+        if i == len(splash):
+            backspace_stdout(clear_std_out(splash_txt))
+            i = 0
+
+    stdout.flush()
+    backspace_stdout(output)
 
 
-def createLog():
-	"""Opens hibLog and appends date and time to the file"""
+def create_log():
+    """Opens hibLog and appends date and time to the file"""
 
-	f = open(hibLog, 'a')
-	t = localtime()
-	s = " ".join([str(strftime("%B/%d/%Y\t %H:%M", t)), '\n'])
-	f.write(s)
-	f.close()
+    f = open(hibLog, 'a')
+    t = localtime()
+    s = " ".join([str(strftime("%B/%d/%Y\t %H:%M", t)), '\n'])
+    f.write(s)
+    f.close()
 
 
 def hibernate():
+    """
+    hib_cmd = r"C:\Windows\System32\rundll32.exe " +
+    "PowrProf.dll,SetSuspendState"
+    """
 
-	"""
-	hib_cmd = r"C:\Windows\System32\rundll32.exe " +
-	"PowrProf.dll,SetSuspendState"
-	"""
-
-	hib_cmd = "shutdown /h"
-	system(hib_cmd)
-	viewToDoList()
+    hib_cmd = "shutdown /h"
+    system(hib_cmd)
 
 
-def main(timeLimit):
-	"""
-	Sets program to sleep for timeLimit minutes
-	Hibernate log is created after sleep with time stamp
-	Calls toDoList to be viewable when resuming from hibernation
-	Sets the computer to hibernate
-	"""
+def main(time_limit):
+    """
+    Sets program to sleep for timeLimit minutes
+    Hibernate log is created after sleep with time stamp
+    Calls toDoList to be viewable when resuming from hibernation
+    Sets the computer to hibernate
 
-	for i in range(int(timeLimit), 0, -1):
-		output = "Time remaining until hibernation: {}".format(str( i))
-		loadingSplash(60, output)
+    :param time_limit: time limit in minutes
+    """
 
-	createLog()
-	hibernate()
+    for i in range(int(time_limit), 0, -1):
+        output = "Time remaining until hibernation: {}".format(str(i))
+        loading_splash(60, output)
+
+    create_log()
+    hibernate()
+    viewToDoList()
 
 
 if __name__ == "__main__":
 
-	#windll.kernel32.SetConsoleCtrlHandler(0, 1)  # disables sigint / ctrl + c
+    # windll.kernel32.SetConsoleCtrlHandler(0, 1)  # disables sigint / ctrl + c
 
-	if(len(argv) < 2):
-		errorAlert("Missing time parameter")
+    if len(argv) < 2:
+        errorAlert("Missing time parameter")
 
-	else:
-		try:
-			main(int(argv[1]))
+    else:
+        try:
+            main(int(argv[1]))
 
-		except ValueError:
-			errorAlert("Argument must be an integer")
+        except ValueError:
+            errorAlert("Argument must be an integer")
