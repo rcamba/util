9c9dd4b67cc51b93e97dab790ef9dc1303c5bd84
Robert <robertCamba@gmail.com>
2016-02-17 22:28:47 -0600

    pep8

--- resources/searchTags.py
+++ resources/searchTags.py
@@ -2,81 +2,82 @@
 *Not using .bat because it reads commas as a separator/delimiter
 """
 from sys import argv, stdin, stdout
-from root import switchParser, printList, setClipboardData, chooseFromList, pipedList
+from root import switchParser, printList, setClipboardData, chooseFromList, \
+    pipedList
 from tag import getFilenameList, getTagList, handleTagSwitch
-from string import strip
 
 
-AVAILABLE_SWITCHES=['s','f','r']
-
-def main(argList):
-	switches = switchParser(argList)
-
-	if 'f' in switches:
-		file = argList[0]
-		tagList=getTagList(file)
-		print tagList
-
-	elif 'r' in switches:
-		res=handleTagSwitch("r", argList=argList)
-		if len(res)==1:
-			fList=map(lambda x: "\""+x+"\"", getFilenameList(res))
-			printList(fList)
-			print res
-
-		elif len(res)>0:
-			chooseFromTags(res)
+AVAILABLE_SWITCHES = ['s', 'f', 'r']
 
 
-	else:
-		tags = map(lambda xStr: xStr.replace(',', ''), argList)
+def main(arg_list):
+    switches = switchParser(arg_list)
 
-		fileList=map(lambda x: "\""+x+"\"", getFilenameList(tags))
+    if 'f' in switches:
+        _file = arg_list[0]
+        tag_list = getTagList(_file)
+        print tag_list
 
-		if 's' in switches:
-			if len(switches['s'])>0:
-				choice=fileList[int (switches['s'])-1]
+    elif 'r' in switches:
+        res = handleTagSwitch("r", argList=arg_list)
+        if len(res) == 1:
+            f_list = map(lambda x: "\"" + x + "\"", getFilenameList(res))
+            printList(f_list)
+            print res
 
-			else:
-				printList( fileList, pressToContinue=stdout.isatty() )
+        elif len(res) > 0:
+            choose_from_tags(res)
 
-				if len(fileList)==1:
-					choice=fileList[0]
+    else:
+        tags = map(lambda x_str: x_str.replace(',', ''), arg_list)
 
-				else:
-					choice=chooseFromList(fileList)
+        file_list = map(lambda x: "\"" + x + "\"", getFilenameList(tags))
 
-			print choice
-			setClipboardData(choice)
+        if 's' in switches:
+            if len(switches['s']) > 0:
+                choice = file_list[int(switches['s']) - 1]
 
-		else:
-			if len(fileList)>0:
-				choice = fileList[len(fileList)-1]
-				setClipboardData(choice)
-			printList( fileList, pressToContinue=stdout.isatty() )
+            else:
+                printList(file_list, pressToContinue=stdout.isatty())
+
+                if len(file_list) == 1:
+                    choice = file_list[0]
+
+                else:
+                    choice = chooseFromList(file_list)
+
+            print choice
+            setClipboardData(choice)
+
+        else:
+            if len(file_list) > 0:
+                choice = file_list[len(file_list) - 1]
+                setClipboardData(choice)
+            printList(file_list, pressToContinue=stdout.isatty())
 
 
-def chooseFromTags(tList):
-	printList(tList)
-	choice=chooseFromList( tList )
-	cfList=  getFilenameList(choice)
-	printList(cfList)
-	choice=chooseFromList( cfList)
-	setClipboardData ( choice)
+def choose_from_tags(t_list):
 
-if __name__=="__main__":
+    printList(t_list)
+    choice = chooseFromList(t_list)
+    cf_list = getFilenameList(choice)
+    printList(cf_list)
+    choice = chooseFromList(cf_list)
+    setClipboardData(choice)
 
 
-	if stdin.isatty() is False:#for using with nf/search
-		print "Piped search"
-		argList = pipedList("".join(map(str, stdin.readlines())))
-		main(argList)
+if __name__ == "__main__":
 
-	elif len(argv) > 1:
-		main(argv[1:])
+    if stdin.isatty() is False:  # for using with nf/search
+        print "Piped search"
+        argList = pipedList("".join(map(str, stdin.readlines())))
+        main(argList)
 
-	else:
+    elif len(argv) > 1:
+        main(argv[1:])
 
-		tList=getTagList()
-		tList.sort()
-		chooseFromTags(tList)
+    else:
+
+        tList = getTagList()
+        tList.sort()
+        choose_from_tags(tList)
