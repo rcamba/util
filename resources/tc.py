25e03a04982c63ddf9f5b5e9a62246e180a7c211
Robert <robertCamba@gmail.com>
2017-05-14 21:07:12 -0500

    -q quick switch for remotes

--- resources/tc.py
+++ resources/tc.py
@@ -6,8 +6,13 @@ if __name__ == "__main__":
 
     cData = getClipboardData()
 
-    for arg in argv[1:]:
-        keyboardType(arg)
-        keyboardType(" ")
+    if len(argv) > 1 and "-q" == argv[1]: # add config option for extra string before text or maybe something like regex pattern
+        keyboardType("remote_client.py -qbit \"")
+        keyboardType(cData)
+        keyboardType("\"")
 
-    keyboardType(cData)
+    else:
+        for arg in argv[1:]:
+            keyboardType(arg)
+            keyboardType(" ")
+        keyboardType(cData)
