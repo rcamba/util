04e88018bf51ac1a78b2a82526224de21764b27f
Robert <robertCamba@gmail.com>
2017-05-22 08:40:03 -0500

    script for retagging if file is moved or renamed

---
+++ scripts/mvtag.py
@@ -0,0 +1,6 @@
+from tag_rename import tag_rename
+from sys import argv
+
+
+if __name__ == "__main__":
+    tag_rename(argv[1], argv[2])
--- util_resources/tag_rename.py
+++ util_resources/tag_rename.py
@@ -1,45 +1,55 @@
-# tag_rename
-
 from sys import argv
 from tag import get_tags_for_file
 from os import rename, path, getcwd
-from tag import add_tags, remove_file_from_tags
+from tag import add_tags, remove_file_from_tags, TagException
 
 
-def tag_rename(orig_name, new_name):
+def tag_rename(orig_name, new_name, verbose=False):
+    if not path.isabs(orig_name):
+        orig_name = path.realpath(path.join(getcwd(), orig_name))
 
-    if path.isfile(orig_name):
-        if path.isabs(orig_name) is False:
-            orig_name = path.join(getcwd(), orig_name)
+    if path.isdir(new_name):
+        new_name = path.realpath(path.join(new_name, path.split(orig_name)[1]))
+    elif not path.isabs(new_name):
+        new_name = path.realpath(path.join(getcwd(), new_name))
 
-        tag_list = get_tags_for_file(orig_name)
-        if len(tag_list) == 0:
-            raise ValueError("No tags found for {}".format(orig_name))
+    if not path.isfile(orig_name):
+        raise IOError("Error: Invalid file {f}.".format(f=orig_name))
+    if orig_name == new_name:
+        raise ValueError("The source and destination are the same.")
 
-        if path.isabs(new_name) is False:
-            new_name = path.join(path.split(orig_name)[0], new_name)
+    tag_list = get_tags_for_file(orig_name)
+    if len(tag_list) == 0:
+        raise ValueError("No tags found for {}".format(orig_name))
 
+    if verbose:
         print "Removing {tl} from {f}\n".format(tl=tag_list, f=orig_name)
-        remove_file_from_tags(tag_list, orig_name)
+    remove_file_from_tags(tag_list, orig_name)
 
-        print "Renaming {src} to {dest}\n".format(src=orig_name, dest=new_name)
+    try:
+        if verbose:
+            print "Renaming {src} to {dest}\n".format(src=orig_name, dest=new_name)
         rename(orig_name, new_name)
 
+    except WindowsError:
+        add_tags(tag_list, orig_name)
+        raise
+
+    try:
         tag_str = ", ".join(map(str, tag_list))
-        print "Tagging {dest} with {tags}\n".format(
-            dest=new_name, tags=tag_str)
+        if verbose:
+            print "Tagging {dest} with {tags}\n".format(dest=new_name, tags=tag_str)
         add_tags(tag_list, new_name)
 
-    else:
-        print "Error: Invalid file for first argument"
-
+    except TagException:
+        rename(new_name, orig_name)
+        add_tags(tag_list, orig_name)
+        raise
 
 if __name__ == "__main__":
 
     if len(argv) == 3:
-        orig_name = argv[1]
-        new_name = argv[2]
-        tag_rename(orig_name, new_name)
+        tag_rename(orig_name=argv[1], new_name=argv[2], verbose=True)
 
     else:
         print "Missing arguments."
