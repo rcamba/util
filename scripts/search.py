97e89cf209594752b459487bc7607480c98e54c1
Robert <robertCamba@gmail.com>
2017-05-25 06:15:16 -0500

    add play_count, refactor arg parsing to support pipes

--- scripts/search.py
+++ scripts/search.py
@@ -1,6 +1,9 @@
+"""
+Using .py because .bat reads commas as a separator/delimiter
+"""
+
 if __name__ == "__main__":
-    from search_tags import create_args, parse_args, do_search
+    from search_tags import create_args, do_search
 
     parser = create_args()
-    args = parse_args(parser)
-    do_search(args)
+    do_search(parser)
--- util_resources/search_tags.py
+++ util_resources/search_tags.py
@@ -1,10 +1,9 @@
-"""
-Not using .bat because it reads commas as a separator/delimiter
-"""
+from sys import argv
+from json import load
 from os import path, getcwd
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 from sys import stdin, stdout
-from root import print_list, set_clipboard_data, choose_from_list, piped_list, error_alert
+from root import print_list, set_clipboard_data, choose_from_list, song_log_file
 from tag import get_files_from_tags, get_tags_for_file, get_tag_by_partial_match, get_mixed_files_from_tags
 
 
@@ -39,40 +38,17 @@ def prune_exceptions_from_file_list(file_list, exception_list):
                 file_list.remove(f)
 
 
-def search(tag_list, exception_list=None):
-    if len(args.search_filename) > 0:
-        if not path.isabs(args.search_filename):
-            args.search_filename = path.join(getcwd(), args.search_filename)
-        args.search_filename = path.realpath(args.search_filename)
-        if not path.isfile(args.search_filename):
-            raise IOError("{f} is not a valid file".format(f=args.search_filename))
-
-        tag_list = get_tags_for_file(args.search_filename)
-        print tag_list
-
-    elif args.partial_match:
-        tag = get_tag_by_partial_match(tag_list[0])
-
-        if tag is None:
-            raise Exception("No tag found matching: " + tag_list[0])
-
-        file_list = get_files_from_tags(tag)
-
-        prune_exceptions_from_file_list(file_list, exception_list)
-        print tag
-        present_result(file_list)
-
+def search_tags(tag_list, exception_list=None):
+    if args.mix_tags:
+        file_list = get_mixed_files_from_tags(tag_list)
     else:
-        if args.mix_tags:
-            file_list = get_mixed_files_from_tags(tag_list)
-        else:
-            file_list = get_files_from_tags(tag_list)
+        file_list = get_files_from_tags(tag_list)
 
-        if len(file_list) == 0:
-            raise Exception("No files found for given tag(s): " + ", ".join(tag_list))
+    if len(file_list) == 0:
+        raise Exception("No files found for given tag(s): " + ", ".join(tag_list))
 
-        prune_exceptions_from_file_list(file_list, exception_list)
-        present_result(file_list)
+    prune_exceptions_from_file_list(file_list, exception_list)
+    present_result(file_list)
 
 
 def choose_from_tags(t_list_):
@@ -94,7 +70,11 @@ def create_args():
 
     p.add_argument("-s", "--select", action="store_true", dest="select")
 
-    group.add_argument("-f", "--file-search", type=str, default="", dest="search_filename")
+    p.add_argument('infile', nargs='?', type=FileType('r'), default=stdin)
+    p.add_argument('outfile', nargs='?', type=FileType('w'), default=stdout)
+
+    group.add_argument("-f", "--file-search", type=str, default="", nargs="?", dest="search_filename")
+    group.add_argument("-pc", "--play-count", type=str, default="", dest="play_count_filename")
     group.add_argument("-r", "--partial", action="store_true", dest="partial_match")
 
     return p
@@ -107,16 +87,69 @@ def parse_args(p):
     return args_
 
 
-def do_search(args_):
+def search_partial_match(partial_tag_str, exception_tags_list):
+    tag = get_tag_by_partial_match(partial_tag_str)
+
+    if tag is None:
+        raise Exception("No tag found matching: " + partial_tag_str)
+
+    file_list = get_files_from_tags(tag)
+
+    prune_exceptions_from_file_list(file_list, exception_tags_list)
+    print tag
+    present_result(file_list)
+
+
+def search_tags_for_file(filename):
+    if not path.isabs(filename):
+        filename = path.join(getcwd(), filename)
+    filename = path.realpath(filename)
+    if not path.isfile(filename):
+        raise IOError("{f} is not a valid file".format(f=filename))
+
+    tag_list = get_tags_for_file(filename)
+    print filename
+    if len(tag_list) == 0:
+        print "No tags"
+    else:
+        print ", ".join(tag_list)
+
+
+def search_for_song_playcount(song_filename):
+    with open(song_log_file) as reader:
+        song_log_dict = load(reader)
+    song_filename = song_filename.replace("\"", "").strip()
+    if song_filename in song_log_dict:
+        print song_filename
+        print song_log_dict[song_filename]["play_count"], "play(s)"
+    else:
+        print song_filename, "not found in songs log."
+
+
+def do_search(parser):
     global args
-    args = args_
+
     if stdin.isatty() is False:  # for using with nf/search
         print "Piped search"
-        arg_list = piped_list("".join(map(str, stdin.readlines())))
-        search(arg_list)
 
-    elif len(args.tags) > 0:
-        search(args.tags, args.exception_tags)
+        piped_fname = stdin.readlines()[2].strip().replace("\"", "")
+        argv.append(piped_fname)
+        loc_args = parser.parse_args(argv)
+    else:
+        loc_args = parse_args(parser)
+
+    args = loc_args
+    if len(loc_args.play_count_filename) > 0:
+        search_for_song_playcount(loc_args.play_count_filename)
+
+    elif len(loc_args.search_filename) > 0:
+        search_tags_for_file(loc_args.search_filename)
+
+    elif loc_args.partial_match:
+        search_partial_match(loc_args.tags[0], loc_args.exception_tags)
+
+    elif len(loc_args.tags) > 0:
+        search_tags(loc_args.tags, loc_args.exception_tags)
 
     else:
         t_list = get_tags_for_file()
@@ -125,7 +158,6 @@ def do_search(args_):
 
 
 if __name__ == "__main__":
-    parser = create_args()
-    args = parse_args(parser)
-    do_search(args)
+    parser_ = create_args()
+    do_search(parser_)
 
