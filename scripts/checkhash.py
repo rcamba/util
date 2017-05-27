79c580e748a66415505be50aab4c1c5e6fd4383f
Robert <robertCamba@gmail.com>
2017-05-27 07:03:35 -0500

    add option to check hash for all files in directory

--- scripts/checkhash.py
+++ scripts/checkhash.py
@@ -1,12 +1,13 @@
-from subprocess import Popen, PIPE
-from sys import argv, stdout, exit as sys_exit
-from hashlib import md5, sha256, sha512
-from string import lower
-from zlib import crc32 as z_crc32
-from os import path as os_path
+import os
+import re
+import sys
+import zlib
+import hashlib
+import subprocess
 
 
 # https://stackoverflow.com/a/5061842
+# noinspection PyPep8Naming
 class crc32(object):
     name = 'crc32'
     digest_size = 4
@@ -28,7 +29,7 @@ class crc32(object):
         return '{:08x}'.format(self.__digest)
 
     def update(self, arg):
-        self.__digest = z_crc32(arg, self.__digest) & 0xffffffff
+        self.__digest = zlib.crc32(arg, self.__digest) & 0xffffffff
 
 
 def raise_invalid_args():
@@ -37,14 +38,14 @@ def raise_invalid_args():
     err_msg += "If checksum is provided then display if it matches " \
                "the calculated checksum"
     print err_msg
-    sys_exit(1)
+    sys.exit(1)
 
 
-def check_hash_certutil(file_, hash_, checksum_val=None):
+def get_hash_value_certutil(fname, hash_algorithm):
     check_hash_command = "CertUtil -hashfile \"{file}\" {hash}".format(
-        file=file_, hash=hash_)
+        file=fname, hash=hash_algorithm)
 
-    proc = Popen(check_hash_command, stdout=PIPE, stderr=PIPE)
+    proc = subprocess.Popen(check_hash_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, _ = proc.communicate()
 
     if "Error" in output:  # since error doesn't get sent to stderr
@@ -52,91 +53,109 @@ def check_hash_certutil(file_, hash_, checksum_val=None):
 
     else:
         checksum_line = output.split('\n')[1]
-        checksum_line = checksum_line.replace(' ', '').strip()
-        print checksum_line
+        checksum = checksum_line.replace(' ', '').strip()
 
-        if checksum_val is not None:
-            print checksum_val
-            if checksum_val == checksum_line:
-                print "Checksum matches"
-            else:
-                print "Checksum does not match"
+    return checksum
 
 
-def check_hash(file_, hash_, checksum_val=None):
+def get_hash_value(fname, hash_algorithm):
+
     hash_func = None
-    bytes_to_read = 4096
+    bytes_to_read = 32768  # 2 ** 15
+    hash_algorithm = hash_algorithm.lower()
 
-    if lower(hash_) == "md5":
-        hash_func = md5()
+    if hash_algorithm in hashlib.algorithms:
+        hash_func = getattr(hashlib, hash_algorithm)()
 
-    elif lower(hash_) == "sha256":
-        hash_func = sha256()
-
-    elif lower(hash_) == "sha512":
-        hash_func = sha512()
-
-    elif lower(hash_) == "crc32":
+    elif hash_algorithm == "crc32":
         hash_func = crc32()
 
-    else:
-        # feed it to certuil as backup
-        print "Hash: {} not found. Feeding to Certutil.".format(hash_)
-        check_hash_certutil(file_, hash_, checksum_val)
-
-    file_size = os_path.getsize(file_)
+    file_size = os.path.getsize(fname) * 1.0
     total_processed = 0
     prev_pct = 0
 
     if hash_func is not None:
-        with open(file_, "rb") as reader:
+        with open(fname, "rb") as reader:
             for chunk in iter(lambda: reader.read(bytes_to_read), b""):
                 total_processed += len(chunk)
                 hash_func.update(chunk)
 
-                curr_pct = int(round(total_processed/(file_size * 1.0), 2) * 100)
-                if prev_pct != curr_pct:
+                curr_pct = int((total_processed / file_size) * 100)
+                if curr_pct % 5 == 0 and prev_pct != curr_pct:
                     prev_pct = curr_pct
-                    update_pct(str(curr_pct) + (1 - len(str(curr_pct))) * " " + "%")
-        stdout.write("\b" * 4)
+                    update_pct(str(curr_pct) + "%")
+        sys.stdout.write("\b" * 4)
         checksum = hash_func.hexdigest()
 
-        # print checksum
-        if checksum_val is not None:
-            print checksum == checksum_val
+    else:
+        # feed it to certuil as backup
+        print "Hash: {} not found. Feeding to Certutil.".format(hash_algorithm)
+        checksum = get_hash_value_certutil(fname, hash_algorithm)
 
     return checksum
 
 
 def update_pct(w_str):
     w_str = str(w_str)
-    stdout.write("\b" * len(w_str))
-    stdout.write(" " * len(w_str))
-    stdout.write("\b" * len(w_str))
-    stdout.write(w_str)
-    stdout.flush()
+    sys.stdout.write("\b" * len(w_str))
+    sys.stdout.write(" " * len(w_str))
+    sys.stdout.write("\b" * len(w_str))
+    sys.stdout.write(w_str)
+    sys.stdout.flush()
+
+
+def check_hash_in_dir(directory_, hash_algorithm="crc32"):
+    f_list = [os.path.join(directory_, f) for f in os.listdir(directory_)]
+    all_matched = True
+    for f in f_list:
+        expected_hash = re.findall("\[\w+\]", f)[-1].lower().replace("[", "").replace("]", "")
+        hash_value = get_hash_value(f, hash_algorithm)
+        match = get_match_str(hash_value, expected_hash)
+        if all_matched and hash_value != expected_hash:
+            all_matched = False
+        print "{f}, {ha}: {hv}{m}".format(f=os.path.split(f)[1],
+                                         ha=hash_algorithm.upper(), hv=hash_value.upper(), m=match)
+
+    print "All matched:", all_matched
+
+
+def get_match_str(hash_value, expected_hash_value):
+    match = ""
+    if expected_hash_value is not None:
+        if expected_hash_value == hash_value:
+            match += " match"
+        else:
+            match += " doesn't match " + expected_hash_value.upper()
+    return match
 
 
 if __name__ == "__main__":
-    hash_ = "MD5"  # default
-    checksum_val = None
+    import time
+    s = time.time()
+    hash_algorithm_ = "MD5"  # default
+    expected_hash_value_ = None
 
-    if len(argv) == 1 or any(map(lambda arg: "help" in arg, argv)):
+    if len(sys.argv) == 1 or any(map(lambda arg: "help" in arg, sys.argv)) or len(sys.argv) > 4:
         raise_invalid_args()
+    elif len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
+        check_hash_in_dir(sys.argv[1])
 
-    elif len(argv) == 3:
-        if len(argv[2]) >= 32:  # if it's long assume it's a checksum...
-            checksum_val = argv[2]
-        else:
-            hash_ = argv[2]
+    else:
+        # if len(sys.argv) == 2 and os.path.isfile(sys.argv[1]):
+        #     hash_value_ = get_hash_value(sys.argv[1], hash_algorithm_)
 
-    elif len(argv) == 4:
-        hash_ = argv[2]
-        checksum_val = argv[3]
+        if len(sys.argv) == 3:
+            if len(sys.argv[2]) > 6:  # hash_algorithmn_ names max len currently is 6 SHA___
+                expected_hash_value_ = sys.argv[2].lower()
+            else:
+                hash_algorithm_ = sys.argv[2]
 
-    elif len(argv) > 4:
-        raise_invalid_args()
+        elif len(sys.argv) == 4:
+            hash_algorithm_ = sys.argv[2]
+            expected_hash_value_ = sys.argv[3].lower()
 
-    file_ = argv[1]
-    print check_hash(file_, hash_, checksum_val)
-    # check_hash_certutil(file_, hash_, checksum_val)
+        fname_ = sys.argv[1]
+        hash_value_ = get_hash_value(fname_, hash_algorithm_)
+        match_ = get_match_str(hash_value_, expected_hash_value_)
+
+        print "{ha}: {hv}{m}".format(ha=hash_algorithm_.upper(), hv=hash_value_.upper(), m=match_)
